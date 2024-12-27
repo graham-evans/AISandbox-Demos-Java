e1da6114f62a21a7781485b72c4bc0cf41c86e39
Graham Evans <gde@pobox.com>
2024-12-27 16:47:05 +0000

    Initial upload

---
+++ HighLowCards-SimpleRules/src/main/java/dev/aisandbox/demo/Launch.java
@@ -0,0 +1,13 @@
+package dev.aisandbox.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class Launch
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
