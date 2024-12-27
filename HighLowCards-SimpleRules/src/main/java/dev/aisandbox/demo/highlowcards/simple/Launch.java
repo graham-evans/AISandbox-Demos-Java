39403089688332e93eba2f81b5a85529c8da18ed
Graham Evans <gde@pobox.com>
2024-12-27 16:47:05 +0000

    correct spelling

--- HighLowCards-SimpleRules/src/main/java/dev/aisandbox/demo/highlowcards/simple/Launch.java
+++ HighLowCards-SimpleRules/src/main/java/dev/aisandbox/demo/highlowcards/simple/Launch.java
@@ -2,6 +2,7 @@ package dev.aisandbox.demo.highlowcards.simple;
 
 import dev.aisandbox.server.simulation.highlowcards.proto.HighLowCardAction;
 import dev.aisandbox.server.simulation.highlowcards.proto.HighLowCardsState;
+import dev.aisandbox.server.simulation.highlowcards.proto.HighLowChoice;
 import dev.aisandbox.server.simulation.highlowcards.proto.Signal;
 
 import java.io.IOException;
@@ -27,19 +28,34 @@ public class Launch {
             // create input and output streams
             OutputStream outputStream = clientSocket.getOutputStream();
             InputStream inputStream = clientSocket.getInputStream();
-            // read the simulation state
-            HighLowCardsState state = HighLowCardsState.parseDelimitedFrom(inputStream);
-            // write current card to screen
-            System.out.println(String.join(":", state.getDeltCardList()));
-            // does the server want a prediction
-            if (state.getSignal().equals(Signal.PLAY)) {
-                // guess the next card
-                HighLowCardAction action = HighLowCardAction.newBuilder()
-                        .setAction(random.nextBoolean() ? HIGH : LOW)
-                        .build();
-                // send this to the server
-                action.writeDelimitedTo(outputStream);
-
+            while (true) {
+                // read the simulation state
+                HighLowCardsState state = HighLowCardsState.parseDelimitedFrom(inputStream);
+                if (state != null) {
+                    // write current card to screen
+                    System.out.println("Got state with " + state.getDealtCardList().size() + " cards");
+                    System.out.println(String.join(":", state.getDealtCardList()));
+                    // does the server want a prediction
+                    if (state.getSignal().equals(Signal.PLAY)) {
+                        // look at the current card and pick high/low
+                        HighLowChoice choice = switch(state.getDealtCardList().getLast().charAt(0)) {
+                            case 'A','2','3','4','5','6' -> HIGH;
+                            case '8','9','1','J','Q','K' -> LOW;
+                            default ->random.nextBoolean() ? HIGH : LOW;
+                        };
+                        // format this as an action
+                        HighLowCardAction action = HighLowCardAction.newBuilder()
+                                .setAction(choice)
+                                .build();
+                        System.out.println("Sending action: " + choice);
+                        // send this to the server
+                        action.writeDelimitedTo(outputStream);
+                    }
+                } else {
+                    System.err.println("Server finished - closing connection");
+                    clientSocket.close();
+                    System.exit(0);
+                }
             }
         } catch (IOException e) {
             System.err.println("Error talking to server");
